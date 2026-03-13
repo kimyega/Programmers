24f261a116d95a9907d6056375cb5eea38551378
kimyega <kimyega@naver.com>
2026-03-13 11:27:56 +0900

    programmers

--- src/main/java/level_0/DiceGame3.java
+++ src/main/java/level_0/DiceGame3.java
@@ -3,6 +3,7 @@ package level_0;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class DiceGame3 {
@@ -19,9 +20,23 @@ public class DiceGame3 {
     }
 
     public static int solution(int a, int b, int c, int d) {
-        int answer = 0;
+        int[] arr = {a, b, c, d};
+        Arrays.sort(arr);
 
+        if (arr[0] == arr[3]) return 1111 * arr[0];
 
-        return answer;
+        if (arr[0] == arr[2] || arr[1] == arr[3]) {
+            int p = arr[1];
+            int q = arr[0] == arr[2] ? arr[3] : arr[0];
+            return (int) Math.pow(10 * p + q, 2);
+        }
+
+        if (arr[0] == arr[1] && arr[2] == arr[3]) return (arr[0] + arr[3]) * (arr[3] - arr[0]);
+
+        if (arr[0] == arr[1]) return arr[2] * arr[3];
+        if (arr[1] == arr[2]) return arr[0] * arr[3];
+        if (arr[2] == arr[3]) return arr[0] * arr[1];
+
+        return arr[0];
     }
 }
