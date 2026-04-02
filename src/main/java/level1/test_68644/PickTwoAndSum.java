2cc5027b143a1980f70b43ac612fa1f5a687c73a
kimyega <kimyega@naver.com>
2026-04-02 15:13:12 +0900

    programmers

---
+++ src/main/java/level1/test_68644/PickTwoAndSum.java
@@ -0,0 +1,36 @@
+package level1.test_68644;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class PickTwoAndSum {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] input = br.readLine().split(" ");
+        int[] numbers = new int[input.length];
+
+        for (int i = 0; i < input.length; i++) {
+            numbers[i] = Integer.parseInt(input[i]);
+        }
+
+        System.out.println(Arrays.toString(solution(numbers)));
+    }
+
+    public static int[] solution(int[] numbers) {
+
+        Set<Integer> set = new TreeSet<>();
+
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = i + 1; j < numbers.length; j++) {
+                set.add(numbers[i] + numbers[j]);
+            }
+        }
+
+        return set.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
