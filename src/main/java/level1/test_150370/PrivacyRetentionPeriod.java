8368dd2050ac45d585ed9b5cc594698f2f9a00f7
kimyega <kimyega@naver.com>
2026-05-08 12:15:11 +0900

    programmers

---
+++ src/main/java/level1/test_150370/PrivacyRetentionPeriod.java
@@ -0,0 +1,56 @@
+package level1.test_150370;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class PrivacyRetentionPeriod {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String today = br.readLine();
+        String[] terms = br.readLine().split(" ");
+        String[] privacies = br.readLine().split(" ");
+
+        System.out.println(Arrays.toString(solution(today, terms, privacies)));
+    }
+
+    private static Map<Character, Integer> map = new HashMap<>();
+
+    public static int[] solution(String today, String[] terms, String[] privacies) {
+        List<Integer> list = new ArrayList<>();
+
+        int todayValue = toDays(today);
+
+        for (int i = 0; i < privacies.length; i++) {
+
+            String[] split = privacies[i].split(" ");
+
+            int privacyDay = toDays(split[0]);
+
+            char type = split[1].charAt(0);
+
+            int month = map.get(type);
+
+            privacyDay += month * 28;
+
+            if (privacyDay <= todayValue) {
+                list.add(i + 1);
+            }
+        }
+
+        return list.stream().mapToInt(Integer::intValue).toArray();
+    }
+
+    public static int toDays(String date) {
+
+        String[] split = date.split("\\.");
+
+        int year = Integer.parseInt(split[0]);
+        int month = Integer.parseInt(split[1]);
+        int day = Integer.parseInt(split[2]);
+
+        return year * 12 * 28 + month * 28 + day;
+    }
+}
