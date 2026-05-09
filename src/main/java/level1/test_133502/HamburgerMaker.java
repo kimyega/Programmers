45157cd91cca4d7c761d7fde87ebb6cb665012e5
kimyega <kimyega@naver.com>
2026-05-09 22:39:08 +0900

    programmers

--- src/main/java/level1/test_133502/HamburgerMaker.java
+++ src/main/java/level1/test_133502/HamburgerMaker.java
@@ -3,6 +3,7 @@ package level1.test_133502;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class HamburgerMaker {
@@ -19,19 +20,29 @@ public class HamburgerMaker {
         System.out.println(solution(ingredient));
     }
 
+
     public static int solution(int[] ingredient) {
-        int answer = 0;
+        int count = 0;
 
-        int[] hamburger = {1, 2, 3, 1};
-        int n = 0;
-        boolean isHamburger = false;
+        Stack<Integer> stack = new Stack<>();
 
-        for (int i = 0; i < ingredient.length; i++) {
-            if (ingredient[i] == ingredient[n]) {
-                n++;
-                isHamburger = true;
+        for (int x : ingredient) {
+            stack.push(x);
+
+            if (stack.size() >= 4) {
+                int size = stack.size();
+
+                if (stack.get(size - 1) == 1 &&
+                    stack.get(size - 2) == 3 &&
+                    stack.get(size - 3) == 2 &&
+                    stack.get(size - 4) == 1) {
+
+                    for (int i = 0; i < 4; i++) stack.pop();
+                    count++;
+                }
             }
         }
-        return answer;
+
+        return count;
     }
 }
